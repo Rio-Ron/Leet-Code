2c68245855caee70a5d320ed139e4ff32fe0a6c1
Rio-Ron <jibon.saha@northsouth.edu>
2022-07-19 00:34:24 +0600

    Create invertBinaryTreeRec.cpp

---
+++ invertBinaryTreeRec.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void invert(TreeNode* root){
+        if(root == NULL) return;
+        swap(root ->left , root ->right);
+            
+        if(root ->left != NULL) invert(root ->left);
+        if(root ->right != NULL) invert(root ->right);
+    }
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        invert(root);
+        
+        return root;
+    }
+};
